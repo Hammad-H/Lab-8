069c8a1534cdcea4fae634564f290b4a8a155454
Hammad-H <hammad.hassan@richmond.edu>
2018-10-24 09:49:18 -0400

    Added Person.cpp and tester.cpp

--- Person.cpp
+++ Person.cpp
@@ -2,12 +2,16 @@
 #define __PERSON_CPP__
 
 #include "Person.h"
+#include <iostream>
 
 Person::Person() {}
+
 Person::Person(const Person& other){}
+
 Person::Person(int urid, std::string netid, std::string lname, std::string fname, 
                int dob_day, int dob_month, int dob_year,
                std::string email, std::string address, long phone){}
+
 Person::~Person(){}
 
 std::string Person::getFirstName()
@@ -15,6 +19,28 @@ std::string Person::getFirstName()
 	return "Inside the getter method for getFirstName";
 }
 
+
 void Person::setFirstName(std::string fname){}
 
+std::string Person::getLastName()
+{
+	return "Inside the getter method for getLastName";
+}
+
+void Person::setLastName(std::string lname){}
+
+
+struct tm Person::getDateOfBirth()
+{
+	struct tm dateOfBirth
+	{
+		dateOfBirth.tm_mday = dobDay;
+		dateOfBirth.tm_mon = dobMonth;
+		dateOfBirth.tm_year = dobYear;	
+	};
+
+	return dateOfBirth;
+}
+
+void Person::setDateOfBirth(){}
 #endif
--- tester.cpp
+++ tester.cpp
@@ -21,15 +21,13 @@ int main()
     p.setFirstName("Lilly");
     std::cout << "First Name:	" << p.getFirstName() << "[Expected: Lilly]" << std::endl;
 
-/*	std::cout << "p.setLastName(\"Luxury\")" << std::endl;
+	std::cout << "p.setLastName(\"Luxury\")" << std::endl;
     p.setLastName("Luxury");
     std::cout << "Last Name:	" << p.getLastName() << "[Expected: Luxury]" << std::endl;
-*/
 
-
-
-
-
+    std::cout << "p.setDateOfBirth(\"25, 10, 1998\")" << std::endl;
+    p.setDateOfBirth(25, 10, 1998);
+    std::cout << "Date of Birth: " << "\nDay: " << p.getDateOfBirth().tm_mday << " [Expected: 25]" << std::endl;
 
 
 }
