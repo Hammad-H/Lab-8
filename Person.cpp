ca774c180919f1c5709e75f14420a01223d06b67
Hammad-H <hammad.hassan@richmond.edu>
2018-10-24 19:35:25 -0400

    Finished tester.cpp

--- Person.cpp
+++ Person.cpp
@@ -32,15 +32,36 @@ void Person::setLastName(std::string lname){}
 
 struct tm Person::getDateOfBirth()
 {
-	struct tm dateOfBirth
-	{
-		dateOfBirth.tm_mday = dobDay;
-		dateOfBirth.tm_mon = dobMonth;
-		dateOfBirth.tm_year = dobYear;	
-	};
+	struct tm dateOfBirth;
+	dateOfBirth.tm_mday = dobDay;
+	dateOfBirth.tm_mon = dobMonth;
+	dateOfBirth.tm_year = dobYear;	
 
 	return dateOfBirth;
 }
 
-void Person::setDateOfBirth(){}
+void Person::setDateOfBirth(int day, int month, int year)
+{
+	dobDay = 0;
+	dobMonth = 0;
+	dobYear = 0;
+}
+
+std::string Person::getAddress(){}
+
+void Person::setAddress(std::string adr) {}
+
+std::string  Person::getNetID(){}
+
+int Person::getURID(){}
+
+std::string  Person::getEmail(){}
+
+long Person::getPhone(){}
+
+void Person::setNetID(std::string netid){}
+void Person::setURID(int urid){}
+void Person::setEmail(std::string email){}
+void Person::setPhone(long number){}
+
 #endif
--- tester.cpp
+++ tester.cpp
@@ -3,8 +3,6 @@
 #include "Faculty.h"
 #include <iostream>
 
-using namespace std;
-
 
 int main() 
 {
@@ -27,7 +25,28 @@ int main()
 
     std::cout << "p.setDateOfBirth(\"25, 10, 1998\")" << std::endl;
     p.setDateOfBirth(25, 10, 1998);
-    std::cout << "Date of Birth: " << "\nDay: " << p.getDateOfBirth().tm_mday << " [Expected: 25]" << std::endl;
+    std::cout << "Date of Birth: " << "\nDay: " << p.getDateOfBirth().tm_mday << " [Expected: 25]" 
+    							   << "\nMonth: " << p.getDateOfBirth().tm_mon << " [Expected: 10]"
+    							   << "\nYear: " << p.getDateOfBirth().tm_year << "[Expected: 1998"
+    							   << std::endl; 
 
+   std::cout << "p.setAddress(\"410 Westhampton Way Richmond VA\")" << std::endl;
+   p.setAddress("410 Westhampton Way Richmond VA");
+   std::cout << "Address: " << p.getAddress() <<" [Expected: 410 Westhampton Way Richmond VA]" << std::endl;
 
+   std::cout << "p.setNetID(\"w7uj\")" << std::endl;
+   p.setNetID("w7uj");
+   std::cout << "NetID: " << p.getNetID() << " [Expected: w7uj]" << std::endl;
+
+   std::cout << "p.setURID(23212)" << std::endl;
+   p.setURID(23212);
+   std::cout << "URID: " << p.getURID() << "[Expected: 23212]" << std::endl;
+
+   std::cout << "p.setEmail(\"Lily.Luxury@gmail.com\")" << std::endl;
+   p.setEmail("Lily.Luxury@gmail.com");
+   std::cout << "Email: " << p.getEmail() << "[Expected: Lily.Luxury@gmail.com]" << std::endl;
+
+   std::cout << "p.setPhone(8043417663)" << std::endl;
+   p.setPhone(8043417663);
+   std::cout << "Phone: " << p.getPhone() << "[Expected: 8043417633]" << std::endl;
 }
